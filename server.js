9eba32cb0b25af7a168e9cd0c9f4c14b62fd7ac5
Mohammad Faizan Gazi <faizangazi336@gmail.com>
2026-04-11 05:11:13 +0530

    Testing full automation

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-﻿const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('<h1>Lab Success!</h1>')); app.listen(3000);
+﻿const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('<h1>Lab Success!.udated by faizan gazi via CI/CD pipeline</h1>')); app.listen(3000);
